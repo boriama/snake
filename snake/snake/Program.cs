c8797fcd56753b4e956435b623879c8fec8a4215
boriama <boriama@yandex.ru>
2018-12-21 16:00:07 +0300

    добавил функции выводана экран символа

---
+++ snake/snake/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snake
+{
+    class Program
+    {
+        static void Draw(int x,int y,string sim)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine(sim);
+        }
+        static void Main(string[] args)
+        {
+            Draw(1, 2, "@");
+            Console.ReadLine();
+        }
+    }
+}
