35dc0781da212090edd46a795ab53db0e88a1b3b
boriama <boriama@yandex.ru>
2018-12-22 19:46:16 +0300

    обучился работать со списками

--- snake/snake/Program.cs
+++ snake/snake/Program.cs
@@ -10,9 +10,34 @@ namespace snake
     {
        
         static void Main(string[] args)
+
+            
         {
-            Point p1 = new Point(5,6,'#');
-            p1.Draw();
+         List<int> numList = new List<int>();
+            numList.Add(1);
+            numList.Add(4);
+            numList.Add(9);
+            foreach(int i in numList)
+            {
+                Console.WriteLine(i);
+
+            }
+
+
+            Point p1 = new Point(5 ,6, '#');
+            Point p2 = new Point(6, 7, '$');
+            Point p3 = new Point(2, 3, '*');
+            Point p4 = new Point(9, 2, '%');
+
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
+            pList.Add(p3);
+            pList.Add(p4);
+
+            pList.RemoveAt(0);
+           
+
             Console.ReadLine();
         }
     }
